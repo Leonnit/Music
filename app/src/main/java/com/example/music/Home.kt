5ab59b2f0ffa4c606c91d9cd1e641c6ab2719270
Leonnit NOMENJANAHARY <leonnit@localhost.localdomain>
2026-03-02 09:20:29 +0300

    Troisieme commite

--- app/src/main/java/com/example/music/Home.kt
+++ app/src/main/java/com/example/music/Home.kt
@@ -72,7 +72,7 @@ fun Topbarre() {
 
                 )
             }
-            Spacer(modifier = Modifier.width(8.dp))
+            Spacer(modifier = Modifier.width(9.dp))
             Box(
                 modifier = Modifier
                     .weight(1f)
